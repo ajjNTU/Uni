e503c47571a545b49bd2a577c67bde7425b7523e
ajjNTU <alex.jeffery2022@my.ntu.ac.uk>
2023-01-30 17:58:48 +0000

    add some notes on where i was, i think

--- stringcalc.py
+++ stringcalc.py
@@ -13,35 +13,36 @@ def string_calc(arithmatic):
     # look for -
     #
     # working notes:
-    # doesn't work just yet, I think leading "-" is causing issues and sometimes it skips "+"
+    # doesn't work just yet, I think leading "-" "+" is causing issues and sometimes it skips "+"
     # the naming of stuff is horrible should probably give variable names to stuff like 2 chars up and down
     # there's so many prints, so I could figure out what it's doing
     # it for loops for each symbol, resolving them and replacing with result
     # which at the end should leave the final result
-    BIDMAS = ['*', '/', '+', '-']
+    bidmas = ['*', '/', '+', '-']
     print(arithmatic)
     pos_neg = 1
     for char in arithmatic:
+        two_char_up = arithmatic[arithmatic.index(char) + 2]
         if char == "*":
             count_up = 0
-            while arithmatic.index(char)+2+count_up < len(arithmatic) and arithmatic[arithmatic.index(char)+2+count_up] not in BIDMAS:
+            while arithmatic.index(char)+2+count_up < len(arithmatic) and two_char_up+count_up not in bidmas:
                 count_up += 1
             minchar = min(arithmatic.index(char)+2+count_up, len(arithmatic))
-            multi1 = arithmatic[arithmatic.index(char)+1:minchar]
-            print("multi1", multi1)
+            multi_up = arithmatic[arithmatic.index(char)+1:minchar]
+            print("multi1", multi_up)
             count_down = 0
             if arithmatic.index(char) == 1:
-                multi2 = arithmatic[0]
+                multi_down = arithmatic[0]
                 #print(multi2)
             else:
-                while arithmatic[arithmatic.index(char)-2-count_down] not in BIDMAS:
+                while arithmatic[arithmatic.index(char)-2-count_down] not in bidmas:
                     count_down += 1
-                multi2 = arithmatic[arithmatic.index(char) - 1 - count_down:arithmatic.index(char)]
-            print("multi2", multi2)
-            replacement = arithmatic[arithmatic.index(multi2):minchar]
+                multi_down = arithmatic[arithmatic.index(char) - 1 - count_down:arithmatic.index(char)]
+            print("multi2", multi_down)
+            replacement = arithmatic[arithmatic.index(multi_down):minchar]
             print("replace", replacement)
-            arithmatic = arithmatic.replace(str(replacement), str(int(multi1) * int(multi2)))
-            print("with", str(int(multi1) * int(multi2)))
+            arithmatic = arithmatic.replace(str(replacement), str(int(multi_up) * int(multi_down)))
+            print("with", str(int(multi_up) * int(multi_down)))
             print(arithmatic)
             arithmatic = arithmatic.replace("--", "+")
             arithmatic = arithmatic.replace("+-", "-")
@@ -52,7 +53,7 @@ def string_calc(arithmatic):
     for char in arithmatic:
         if char == "/":
             count_up = 0
-            while arithmatic.index(char)+2+count_up < len(arithmatic) and arithmatic[arithmatic.index(char)+2+count_up] not in BIDMAS:
+            while arithmatic.index(char)+2+count_up < len(arithmatic) and arithmatic[arithmatic.index(char)+2+count_up] not in bidmas:
                 count_up += 1
             minchar = min(arithmatic.index(char)+2+count_up, len(arithmatic))
             div1 = arithmatic[arithmatic.index(char)+1:minchar]
@@ -62,7 +63,7 @@ def string_calc(arithmatic):
                 div2 = arithmatic[0]
                 print(div2)
             else:
-                while arithmatic[arithmatic.index(char)-2-count_down] not in BIDMAS:
+                while arithmatic[arithmatic.index(char)-2-count_down] not in bidmas:
                     count_down += 1
                 div2 = arithmatic[arithmatic.index(char)-1-count_down:arithmatic.index(char)]
             print(div2)
@@ -71,11 +72,11 @@ def string_calc(arithmatic):
             arithmatic = arithmatic.replace(str(replacement), str(int(int(div2) / int(div1))))
             print(arithmatic)
     for char in arithmatic:
-        #print("working on ", char, "index", arithmatic.index(char))
+        #print("working on addition", char, "index", arithmatic.index(char))
         if char == "+":
-            print("char is", char, "index is", arithmatic.index(char))
+            print("char is addition", char, "index is", arithmatic.index(char))
             count_up = 0
-            while arithmatic.index(char)+2+count_up < len(arithmatic) and arithmatic[arithmatic.index(char)+2+count_up] not in BIDMAS:
+            while arithmatic.index(char)+2+count_up < len(arithmatic) and arithmatic[arithmatic.index(char)+2+count_up] not in bidmas:
                 count_up += 1
             minchar = min(arithmatic.index(char)+2+count_up, len(arithmatic))
             add1 = arithmatic[arithmatic.index(char)+1:minchar]
@@ -84,7 +85,7 @@ def string_calc(arithmatic):
             if arithmatic.index(char) == 1:
                 add2 = arithmatic[0]
             else:
-                while arithmatic.index(char)-2-count_down < 0 and arithmatic[arithmatic.index(char)-2-count_down] not in BIDMAS:
+                while arithmatic.index(char)-2-count_down < 0 and arithmatic[arithmatic.index(char)-2-count_down] not in bidmas:
                     count_down += 1
                 if arithmatic[arithmatic.index(char)-2-count_down] == "-":
                     add2 = arithmatic[arithmatic.index(char) - 2 - count_down:arithmatic.index(char)]
@@ -109,10 +110,10 @@ def string_calc(arithmatic):
             print(arithmatic)
             arithmatic = arithmatic.replace("++","+")
             print(arithmatic)
-    for char in arithmatic[1:]:
+    for char in arithmatic:
         if char == "-":
             count_up = 0
-            while arithmatic.index(char)+2+count_up < len(arithmatic) and arithmatic[arithmatic.index(char)+2+count_up] not in BIDMAS:
+            while arithmatic.index(char)+2+count_up < len(arithmatic) and arithmatic[arithmatic.index(char)+2+count_up] not in bidmas:
                 count_up += 1
             minchar = min(arithmatic.index(char)+2+count_up, len(arithmatic))
             sub1 = arithmatic[arithmatic.index(char)+1:minchar]
@@ -121,7 +122,7 @@ def string_calc(arithmatic):
             if arithmatic.index(char) == 1:
                 sub2 = arithmatic[0]
             else:
-                while arithmatic.index(char)-2-count_down < 0 and arithmatic[arithmatic.index(char)-2-count_down] not in BIDMAS:
+                while arithmatic.index(char)-2-count_down < 0 and arithmatic[arithmatic.index(char)-2-count_down] not in bidmas:
                     count_down += 1
                 sub2 = arithmatic[arithmatic.index(char)-1-count_down:arithmatic.index(char)]
             print(sub2)
