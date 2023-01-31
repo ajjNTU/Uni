95779274a26e5cf129af5e329277716e230e5067
ajjNTU <alex.jeffery2022@my.ntu.ac.uk>
2023-01-31 14:52:22 +0000

    exponents work?

--- stringcalc.py
+++ stringcalc.py
@@ -8,17 +8,16 @@ def string_calc(arithmatic):
     #   move to next brack
     # no more brackets
     # IDMAS:
-    # look for **, maybe use ^ instead so replace ** to ^? just to deal with single character
+    # look for **
     # look for *
     # look for /
     # look for +
     # look for -
     #
     # working notes:
-    # doesn't like floats so any remainder just gets 0'd. could maybe add a running total of remainders but
-    #       division doesn't look at the sign of the first number
-    # ignoring brackets and powers for now
-    # brackets could use string calc to do calc inside bracket then string calc that? recursive like??
+    # added exponents but it's messsy
+    # doesn't like floats so any remainder from division just gets 0'd
+    # ignoring brackets
     #
 
     def counting_up():
@@ -28,7 +27,7 @@ def string_calc(arithmatic):
             count_up += 1
         min_char = min(arithmatic.index(char) + 2 + count_up, len(arithmatic))
         counting_up_string = arithmatic[arithmatic.index(char) + 1:min_char]
-        # print("up string:", counting_up_string)
+        print("up string:", counting_up_string)
         return counting_up_string
 
     def counting_down():
@@ -40,12 +39,49 @@ def string_calc(arithmatic):
                     and arithmatic[arithmatic.index(char) - 2 - count_down] not in bidmas:
                 count_down += 1
             counting_down_string = arithmatic[arithmatic.index(char) - 1 - count_down:arithmatic.index(char)]
-        # print("down_string:" + counting_down_string)
+        print("down_string:" + counting_down_string)
         return counting_down_string
 
-    bidmas = ['*', '/', '+', '-']
-    arithmatic.replace(" ", "")
+    bidmas = ['^', '*', '/', '+', '-']
+    arithmatic = arithmatic.replace(" ", "")
+    arithmatic = arithmatic.replace("**", "^")
     print(f"Computing: {arithmatic}")
+    # this might be better than my for loops below, this one works backwards but others would go forward?
+    while arithmatic.__contains__("^"):
+        max_power_index = arithmatic.rfind("^")
+        count_up = 0
+        while max_power_index + 2 + count_up < len(arithmatic) \
+                and arithmatic[max_power_index + 2 + count_up] not in bidmas:
+            count_up += 1
+        min_char = min(max_power_index + 2 + count_up, len(arithmatic))
+        up_string_exp = arithmatic[max_power_index + 1:min_char]
+        count_down = 0
+        if max_power_index == 1:
+            down_string_exp = arithmatic[0]
+        else:
+            while max_power_index - 1 - count_down > 0 \
+                    and arithmatic[max_power_index - 2 - count_down] not in bidmas:
+                count_down += 1
+            down_string_exp = arithmatic[max_power_index - 1 - count_down:max_power_index]
+        if arithmatic[arithmatic.index(str(down_string_exp) + "^") - 1] == "-":
+            down_string_exp = "-" + down_string_exp
+            new_string_exp = int(down_string_exp) ** int(up_string_exp)
+        else:
+            new_string_exp = int(down_string_exp) ** int(up_string_exp)
+        to_replace = f"{down_string_exp}^{up_string_exp}"
+        # if down string was negative, and we end up with a positive need to replace the - with +
+        if int(down_string_exp) < 0 < int(new_string_exp):
+            arithmatic = arithmatic.replace(str(to_replace), "+" + str(new_string_exp))
+            print(f"replace {to_replace} with +{new_string_exp}")
+        else:
+            arithmatic = arithmatic.replace(str(to_replace), str(new_string_exp))
+            print(f"replace {to_replace} with {new_string_exp}")
+        # some checks remove leading "+" or double operator
+        arithmatic = arithmatic.replace("--", "+")
+        arithmatic = arithmatic.replace("+-", "-")
+        if arithmatic[0] == "+":
+            arithmatic = arithmatic[1:]
+        print(f"New arithmatic: {arithmatic}")
     while arithmatic.__contains__("*"):
         for char in arithmatic:
             if char == "*":
@@ -54,13 +90,13 @@ def string_calc(arithmatic):
                 to_replace = f"{down_string}*{up_string}"
                 new_string_multi = int(int(down_string) * int(up_string))
                 arithmatic = arithmatic.replace(str(to_replace), str(new_string_multi))
-                # print(f"replace {to_replace} with {new_string_multi}")
+                print(f"replace {to_replace} with {new_string_multi}")
                 # some checks remove leading "+" or double operator
                 arithmatic = arithmatic.replace("--", "+")
                 arithmatic = arithmatic.replace("+-", "-")
                 if arithmatic[0] == "+":
                     arithmatic = arithmatic[1:]
-                # print(f"New arithmatic: {arithmatic}")
+                print(f"New arithmatic: {arithmatic}")
     while arithmatic.__contains__("/"):
         for char in arithmatic:
             if char == "/":
@@ -69,20 +105,20 @@ def string_calc(arithmatic):
                 to_replace = f"{down_string}/{up_string}"
                 new_string_div = int(int(down_string) / int(up_string))
                 arithmatic = arithmatic.replace(str(to_replace), str(new_string_div))
-                # print(f"replace {to_replace} with {new_string_div}")
+                print(f"replace {to_replace} with {new_string_div}")
                 # some checks remove leading "+" or double operator
                 arithmatic = arithmatic.replace("--", "+")
                 arithmatic = arithmatic.replace("+-", "-")
                 if arithmatic[0] == "+":
                     arithmatic = arithmatic[1:]
-                # print(f"New arithmatic: {arithmatic}")
+                print(f"New arithmatic: {arithmatic}")
     while arithmatic.__contains__("+"):
         for char in arithmatic:
             if char == "+":
                 up_string = counting_up()
                 down_string = counting_down()
                 # look if down_string should be -
-                if arithmatic[arithmatic.index(down_string) - 1] == "-":
+                if arithmatic[arithmatic.index(str(down_string) + "+") - 1] == "-":
                     down_string = "-" + down_string
                     new_string_add = int(down_string) + int(up_string)
                 else:
@@ -91,16 +127,16 @@ def string_calc(arithmatic):
                 # if down string was negative, and we end up with a positive need to replace the - with +
                 if int(down_string) < 0 < int(new_string_add):
                     arithmatic = arithmatic.replace(str(to_replace), "+" + str(new_string_add))
-                    # print(f"replace {to_replace} with +{new_string_add}")
+                    print(f"replace {to_replace} with +{new_string_add}")
                 else:
                     arithmatic = arithmatic.replace(str(to_replace), str(new_string_add))
-                    # print(f"replace {to_replace} with {new_string_add}")
+                    print(f"replace {to_replace} with {new_string_add}")
                 # some checks remove leading "+" or double operator
                 arithmatic = arithmatic.replace("--", "+")
                 arithmatic = arithmatic.replace("+-", "-")
                 if arithmatic[0] == "+":
                     arithmatic = arithmatic[1:]
-                # print(f"New arithmatic: {arithmatic}")
+                print(f"New arithmatic: {arithmatic}")
     final = 0
     if not arithmatic[1:].isnumeric():
         if arithmatic[0] == "-":
@@ -118,19 +154,20 @@ def string_calc(arithmatic):
     return final
 
 
-string_calc("4+4")
-string_calc("-3*-3")
-string_calc("3*-3")
-string_calc("-3*3")
-string_calc("3/-1+5-4*10000*-10")
-string_calc("1+3+5-4*10/2")
-string_calc("1-3+5-4000*10000+2*2")
-string_calc("1-3+5-4000*10000+2*2*-2/-2")
-string_calc("1-3+5+4000*10000-2*2*-2/2")
-string_calc("1-3+5+4000*10000+2*2*2/-2")
-string_calc("1-3+5-4000*10000+2*2*2/-7")
-string_calc("1-3+5-4000*10000+6/-7")
-string_calc("1-3+5-4000*10000+3/-7")
-string_calc("1-3+5-4000*10000+3/-7+2/7")
-string_calc("1-3+5+4000*10000+3/-7+2/7-17/7")
-
+# string_calc("4+4")
+# string_calc("-3*-3")
+# string_calc("3*-3")
+# string_calc("-3*3")
+# string_calc("3/-1+5-4*10000*-10")
+# string_calc("1+3+5-4*10/2")
+# string_calc("1-3+5-4000*10000+2*2")
+# string_calc("1-3+5-4000*10000+2*2*-2/-2")
+# string_calc("1-3+5+4000*10000-2*2*-2/2")
+# string_calc("1-3+5+4000*10000+2*2*2/-2")
+# string_calc("1-3+5-4000*10000+2*2*2/-7")
+# string_calc("1-3+5-4000*10000+6/-7")
+# string_calc("1-3+5-4000*10000+3/-7")
+# string_calc("1-3+5-4000*10000+3/-7+2/7")
+# string_calc("1-3+5+4000*10000+3/-7+2/7-17000/7**3")
+# string_calc("1-3+5+4000*10000+3/-7+2/7-17000/7^3")
+string_calc("1-3+5+4000*10000+3/-7+2/7-17000**15/-7**3**3")
